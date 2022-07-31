06d3629fcc62b3fcbdb24ca38547b19cbe087dda
Taylor Masiello <masiellotay@gmail.com>
2022-07-30 22:21:53 -0700

    Created screen buffer; Created play field, initialized and set to empty; Created game loop; Draws frame in console successfully;

--- Tetris_NoGraphics/Tetris_NoGraphics.cpp
+++ Tetris_NoGraphics/Tetris_NoGraphics.cpp
@@ -2,10 +2,17 @@
 //
 
 #include <iostream>
+#include <Windows.h>
 
 std::wstring tetromino[7];
+int nFieldWidth = 12;           //store elements of field as an unsigned char[]; all map info in single array
+int nFieldHeight = 18;
+unsigned char* pField = nullptr; //will be allocated dynamically
 
-int Rotate(int px, int py, int r) // takes x,y (px,py) and calculates where shape exists within array+possible rotation (r as 0,1,2,3)
+int nScreenWidth = 120;          //Console Screen Size X (columns) (default X,Y of windows command prompt)
+int nScreenHeight = 30;         //Console Screen Size Y (rows)
+
+int Rotate(int px, int py, int r) //takes x,y (px,py) and calculates where shape exists within array+possible rotation (r as 0,1,2,3), returns appropriate index 
 {
     switch (r % 4)
     {
@@ -14,10 +21,19 @@ int Rotate(int px, int py, int r) // takes x,y (px,py) and calculates where shap
     case 2: return 15 - (py * 4) - px; //180 degrees
     case 3: return 3 - py + (px * 4);  //270 degrees
     }
+
+    return 0;
 }
 
 int main()
 {
+    //Create Screen Buffer (effectively use command line as a screen buffer, Creates wchar_t[] for screen width,height (X,Y), filled with blanks)
+    wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+    for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);   //Grabs HANDLE to consoleBuffer
+    SetConsoleActiveScreenBuffer(hConsole);       //sets to activeScreenBuffer (cannot use cout, will have to use a seperate command to draw to the buffer
+    DWORD dwBytesWritten = 0;
+
     //Create assets
     tetromino[0].append(L"..X."); //4x4 array of "yes or no"; 4 elements wide by 4 lines deep
     tetromino[0].append(L"..X."); //uses a "full-stop symbol" . to represent empty space and a captial X to represent part of the shape
@@ -53,6 +69,36 @@ int main()
     tetromino[6].append(L".XX.");
     tetromino[6].append(L".X..");
     tetromino[6].append(L".X..");
+
+    pField = new unsigned char[nFieldWidth * nFieldHeight]; //(Create play field buffer) initializes field[], sets everything to 0 (empty space) unless on side || bottom [] boundary
+    for (int x = 0; x < nFieldWidth; x++)    //Board Boundary
+        for (int y = 0; y < nFieldHeight; y++)
+            pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0; //if x boundary == 0, or it's width || height of field, set to 9 (boundary value)
+
+
+
+
+
+    //Game Loop
+    bool bGameOver = false;
+
+    while (!bGameOver)
+    {
+        //Draw Field (whole board [bouandaries, empty spaces, tetrominos])
+        //iterates thru X&Y, offsets by 2 (to avoid drawing exactly topleft corner, helps show boundary)
+        //sets output based on char in string[] (if in field[] value is: 0 will draw ' ', 1-7 for tetrominos will draw as A-G, 8 makes a line as = , border as #)
+        //takes whatever position is in field[], that gives a value that's used to index into string[] to determine what's dispalyed on screen
+        for (int x = 0; x < nFieldWidth; x++) 
+            for (int y = 0; y < nFieldHeight; y++)
+                screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]]; 
+
+        //Display Frame; draws screen[] & dimensions starting at position 0,0 to console
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+    }
+
+
+
+    
 
     return 0;
 }
